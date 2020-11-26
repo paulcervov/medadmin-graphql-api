9b40054d562c12347268c8c012404810e4cb679a
Paul Cervov <paulcervov@gmail.com>
2020-11-27 00:42:44 +0300

    Remove email field from user, rearrange fields in migration, seeder and model

--- src/migrations/20201111214046_create_users_table.js
+++ src/migrations/20201111214046_create_users_table.js
@@ -8,21 +8,18 @@ exports.up = function(knex) {
         table.string('middleName').notNullable();
         table.string('lastName').notNullable();
 
-        table.string('email').notNullable().unique();
-        table.string('password').notNullable();
-
         table.string('phone').notNullable().unique();
+        table.string('password').notNullable();
 
         table.integer('genderId').notNullable();
         table.date('dateOfBirth').notNullable();
-        table.string('comment');
+        table.integer('roleId').notNullable();
 
+        table.integer('percentage');
+        table.string('comment');
         table.json('passport');
         table.json('registrationAddress');
         table.json('physicalAddress');
-
-        table.integer('roleId').notNullable();
-        table.integer('percentage').notNullable();
 
         table.timestamp('createdAt', {useTz: false}).defaultTo(knex.fn.now()).notNullable();
         table.timestamp('updatedAt', {useTz: false}).defaultTo(knex.fn.now()).notNullable();
